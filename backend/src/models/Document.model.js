42d0abe8fe26155ecee24d8d05a55d0fb956d1dc
Nandani Kirtani <160708805+Nandanikirtani@users.noreply.github.com>
2025-07-16 16:49:53 +0530

    User and Video models

---
+++ backend/src/models/Document.model.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+
+const documentSchema = new Schema({
+  uploadedBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  fileName: { type: String, required: true },
+  fileUrl: { type: String, required: true },
+  uploadDate: { type: Date, default: Date.now },
+});
+
+export const Document = mongoose.model("Document", documentSchema);
