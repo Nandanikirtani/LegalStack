8c423eb1ea0cffe20d5ad2d6ab7200cc33fc870b
Nandani Kirtani <160708805+Nandanikirtani@users.noreply.github.com>
2025-07-18 23:22:57 +0530

    Setup of routers and controllers

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,4 +15,10 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+//routes
+import userRouter from "./routes/User.routes.js"
+
+//declaration
+app.use("/api/v1/user",userRouter)
 export {app}
